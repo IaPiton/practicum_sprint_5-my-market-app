290d770f392a55972ff27a6d64fd3a63369e48ee
Ilya.Kukushkin <2Q!4@#$1132brF>
2026-04-10 13:27:10 +0300

    Добавлено логирование ошибок

---
+++ src/main/java/ru/yandex/practicum/my_market_app/api/model/ErrorResponse.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.my_market_app.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String error;
+    private String message;
+}
